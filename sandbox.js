62a29e47434563005fac98a04eb5a21d454344a3
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-19 12:00:58 +0530

    refactor: add arrays

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,9 @@
-var chrt = 'mario';
-console.log(chrt);
+// Arrays
+// Initial data type is string.
+var names = ['luigi', 'mario', 'yoshi'];
+// add element(name) to array
+names.push('toad');
+// names.push(3);                 // error gives because daya type is string. now we can't change the data type of array after initialization.
+names[0] = 'peach'; // change the element of array
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,13 @@
-let chrt = 'mario';
+// Arrays
+// Initial data type is string.
+let names = ['luigi', 'mario', 'yoshi'];
 
-console.log(chrt);
+// add element(name) to array
+names.push('toad');
+// names.push(3);                 // error gives because daya type is string. now we can't change the data type of array after initialization.
+names[0] = 'peach'; // change the element of array
+
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
