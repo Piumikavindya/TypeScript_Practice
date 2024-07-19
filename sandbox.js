808a4799f7cbc60dd120df076e88f5aee894cd51
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-19 12:24:17 +0530

    refactor: add array with string initialization

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,10 @@
-var chrt = 'mario';
-console.log(chrt);
+// explicit types
+// intialize the data type for each varible
+var character;
+var age;
+var isLoggedIn;
+//  age = 'lugi';    // this is giving an error because age is a number and we are trying to assign a string to it
+age = 30;
+// Arrays
+var ninjas = []; // initailize an empty array of strings
+ninjas.push('piumi'); // add a string to the array
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,16 @@
-let chrt = 'mario';
+// explicit types
+// intialize the data type for each varible
+    let character: string;
+    let age: number;
+    let isLoggedIn: boolean;
 
-console.log(chrt);
+  //  age = 'lugi';    // this is giving an error because age is a number and we are trying to assign a string to it
+  age = 30;
+
+
+  // Arrays
+
+  let ninjas: string[]= []; // initailize an empty array of strings
+
+  ninjas.push('piumi'); // add a string to the array
+
