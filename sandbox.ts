5c7d20d42154c962b60282938f16ddcf1ae139a9
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-19 09:49:10 +0530

    add different variables

--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,15 @@
-let chrt = 'mario';
+// no need to specify what is the data type. typescript will automatically detect the data type
+let character = 'mario';
+let age = 10;
+let isBlackBelt = false;
 
-console.log(chrt);
+// character = 20; // error
+character = 'piumi' // no error
+
+// in typescript we cant change the data type of a variable. we can change value but not data type
+
+// age = 'yoshi'; // error
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
