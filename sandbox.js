ef64dd0cb0cd72e2176a739dbe19f5a2605a2465
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-19 12:47:42 +0530

    refactor: add 'any' datatype for properties of object

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,11 @@ age = true;
 console.log(age);
 age = 'hello';
 console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+var ninja; // here not giveing values to properties. here assign any type of data type to name and age
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -5,3 +5,16 @@
     console.log(age);
     age = 'hello';
     console.log(age);
+
+
+    let mixed: any[] = [];
+
+    mixed.push(5);
+    mixed.push('mario');
+    mixed.push(false);
+    console.log(mixed);
+
+    let ninja: { name: any, age: any };  // here not giveing values to properties. here assign any type of data type to name and age
+
+    ninja = { name: 'yoshi', age: 25 };
+    console.log(ninja);
