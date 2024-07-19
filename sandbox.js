45725ef55dd298390ce61a05853cb551dcfb28fe
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-19 09:57:10 +0530

    add circ function

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,15 @@
-var chrt = 'mario';
-console.log(chrt);
+// no need to specify what is the data type. typescript will automatically detect the data type
+var character = 'mario';
+var age = 10;
+var isBlackBelt = false;
+// character = 20; // error
+character = 'piumi'; // no error
+// in typescript we cant change the data type of a variable. we can change value but not data type
+// age = 'yoshi'; // error
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -13,3 +13,10 @@ age = 40;
 
 // isBlackBelt = 'yes';
 isBlackBelt = true;
+
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5));
