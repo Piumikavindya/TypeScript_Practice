dab87b29d7e3b989cbb1d726e1f20e783120095e
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-20 15:14:12 +0530

    refactor: create functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
-var chrt = 'mario';
-console.log(chrt);
+"use strict";
+let greet;
+// greet = 'hello'; // Error
+greet = () => {
+    console.log('Hello, world!');
+};
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+// no required to give return type
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,22 @@
-let chrt = 'mario';
+let greet: Function;
 
-console.log(chrt);
+// greet = 'hello'; // Error
+
+greet =() => {
+    console.log('Hello, world!');
+}
+
+const add = (a: number, b: number, c?: number | string) =>{
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, '20');
+
+// no required to give return type
+const minus = (a: number, b: number): number => {
+
+    return a+b;
+}
+
+let result = minus(10, 7);
