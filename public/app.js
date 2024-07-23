2ee75b9249644018db5566c7fbdbb9e97d8282a4
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-23 10:55:50 +0530

    refactor: add objects of Invoice class and pass the parameters

--- public/app.js
+++ public/app.js
@@ -1,15 +1,21 @@
 "use strict";
 // Classes
 class Invoice {
+    // constructor
     constructor(c, d, a) {
         this.client = c;
         this.details = d;
         this.amount = a;
     }
+    // Method
     format() {
         return `${this.client} owes Rs${this.amount} for ${this.details}`;
     }
 }
+// create instance/object of Invoice class and pass the parameters
+const invOne = new Invoice('piumi', 'work on the website', 250);
+const invTwo = new Invoice('Kavindya', 'work on the website', 200);
+console.log(invOne, invTwo);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,30 @@
 // Classes
 
 class Invoice{
+    // variables
     client: string;
     details: string;
     amount: number;
 
+    // constructor
     constructor(c: string, d: string, a:number){
         this.client = c;
         this.details = d;
         this.amount = a;
     }
     
+    // Method
     format(){
         return `${this.client} owes Rs${this.amount} for ${this.details}`;
     }
 }
+
+// create instance/object of Invoice class and pass the parameters
+const invOne = new Invoice('piumi', 'work on the website', 250);
+const invTwo = new Invoice('Kavindya', 'work on the website', 200);
+
+console.log(invOne, invTwo);
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
