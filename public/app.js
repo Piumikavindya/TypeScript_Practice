441765f04ad6dbd89b5ec4e143eab03f05bc90d8
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-23 10:31:47 +0530

    refactor: add event listner to get input values add print it in console

--- public/app.js
+++ public/app.js
@@ -4,7 +4,13 @@
 //     console.log(anchor.href); // http://localhost:1234/
 // }
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
+// console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
-const toFrom = document.querySelector('account');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
