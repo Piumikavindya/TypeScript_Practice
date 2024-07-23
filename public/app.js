0a2cb6eb456b2473f471cc146aaa36f5e5179366
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-23 10:22:41 +0530

    refactor: add Html elemnts based on id or class name

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
 "use strict";
+// const anchor = document.querySelector('a');
+// if(anchor){
+//     console.log(anchor.href); // http://localhost:1234/
+// }
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('account');
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
-const anchor = document.querySelector('a');
+// const anchor = document.querySelector('a');
 
-if(anchor){
-    console.log(anchor.href); // http://localhost:1234/
-}
+// if(anchor){
+//     console.log(anchor.href); // http://localhost:1234/
+// }
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+console.log(form.children);
+
+// inputs
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('account') as HTMLInputElement;
